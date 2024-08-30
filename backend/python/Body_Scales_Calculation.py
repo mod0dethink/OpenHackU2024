bf52772bba82972a4840376bd06e129019caa5e0
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2024-08-30 23:54:58 +0900

    エラー文探し

--- backend/python/Body_Scales_Calculation.py
+++ backend/python/Body_Scales_Calculation.py
@@ -8,6 +8,31 @@ Width = 40
 #P_Hight = 175
 #P_Width = 48
 
+P_Hight = None
+P_Width = None
+
+# コマンドライン引数が十分にあるか確認
+if len(sys.argv) > 3:
+    try:
+        # sys.argv[3] の値を float に変換し、Width に代入
+        P_Width = float(sys.argv[4])
+        print(f"Width に代入された値: {P_Width}")
+    except ValueError:
+        print("指定された値が数値に変換できません。")
+else:
+    print("コマンドライン引数が不足しています。")
+
+if len(sys.argv) > 3:
+    try:
+        # sys.argv[3] の値を float に変換し、Width に代入
+        P_Hight = float(sys.argv[3])
+        print(f"Width に代入された値: {P_Hight}")
+    except ValueError:
+        print("指定された値が数値に変換できません。")
+else:
+    print("コマンドライン引数が不足しています。")
+
+
 P_Hight =  float(sys.argv[3])
 P_Width =  float(sys.argv[4])
 
